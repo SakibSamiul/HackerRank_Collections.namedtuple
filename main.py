71814e3eccf841e14bd9ef34cde4d83c44a64b5f
SakibSamiul <sb.sakib77@gmail.com>
2024-08-17 21:51:18 +0600

    collections.namedtuple

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from collections import namedtuple
+
+def avarage():
+
+    totalmarks = 0
+    for _ in range(N):
+        Student = namedtuple('Student', spreadsheets)
+
+        ID, MARKS, NAME, CLASS = input().split()
+        students = (Student(ID, MARKS, NAME, CLASS))
+        totalmarks += int(students.MARKS)
+
+    avarage = totalmarks / N
+    print(avarage)
+
+if __name__ == '__main__':
+    N = int(input())
+    spreadsheets = input().split()
+    avarage()
